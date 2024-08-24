4335b945dc7448aaf3fc7fdacac4d38817edbffe
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-08-24 17:32:54 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\EventController;
+
+Route::get('/', [EventController::class, 'index']);
+
+Route::get('/events/create', [EventController::class, 'create']);
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
