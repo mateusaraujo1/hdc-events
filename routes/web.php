8ce5d8cea9bed117f0a9d111ee6d1304a2f3c819
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-08-30 15:58:11 -0300

    add delete option

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/', [EventController::class, 'index']);
 
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 
+Route::delete('events/{id}', [EventController::class, 'destroy']);
+
 Route::get('/events/{id}', [EventController::class, 'show']);
 
 Route::post('/events', [EventController::class, 'store']);
