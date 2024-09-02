c62ebf9653f7623a639dd2bcb131b2e326475a58
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2024-09-01 21:52:23 -0300

    database atualizado

--- hdcevents.sql
+++ hdcevents.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29/08/2024 às 02:22
+-- Tempo de geração: 02/09/2024 às 02:51
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -33,6 +33,14 @@ CREATE TABLE `cache` (
   `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Despejando dados para a tabela `cache`
+--
+
+INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
+('0822bda9ca2e316ef1ca873d7029702f', 'i:1;', 1725137086),
+('0822bda9ca2e316ef1ca873d7029702f:timer', 'i:1725137086;', 1725137086);
+
 -- --------------------------------------------------------
 
 --
@@ -59,11 +67,43 @@ CREATE TABLE `events` (
   `description` text NOT NULL,
   `city` varchar(255) NOT NULL,
   `private` tinyint(1) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`items`)),
+  `image` varchar(255) NOT NULL DEFAULT 'event.jpg',
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `date` datetime NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `events`
+--
+
+INSERT INTO `events` (`id`, `created_at`, `updated_at`, `title`, `description`, `city`, `private`, `image`, `items`, `date`, `user_id`) VALUES
+(1, '2024-08-29 04:04:25', '2024-08-31 01:15:32', 'SECOMP', 'aaaabbb', 'Sobral', 0, 'd253cb6436170f15b90d8999f1e363fb', '[\"Palco\"]', '2024-08-30 00:00:00', 1),
+(3, '2024-08-31 23:03:47', '2024-08-31 23:43:04', 'SANA', 'evento de animes', 'Acaraú', 1, 'event.jpg', '[\"Cadeiras\",\"Open food\"]', '2024-09-07 00:00:00', 2),
+(4, '2024-09-01 02:09:33', '2024-09-01 02:09:33', 'aaa', 'fasdf', 'teste', 0, 'event.jpg', NULL, '2024-09-07 00:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `event_user`
+--
+
+CREATE TABLE `event_user` (
+  `event_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `event_user`
+--
+
+INSERT INTO `event_user` (`event_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 2, NULL, NULL),
+(3, 2, NULL, NULL),
+(1, 1, NULL, NULL),
+(3, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,7 +131,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2024_08_28_184112_add_two_factor_columns_to_users_table', 1),
 (8, '2024_08_28_184207_create_personal_access_tokens_table', 1),
 (9, '2024_08_28_223712_create_cache_table', 1),
-(10, '2024_08_28_231221_add_user_id_to_events_table', 1);
+(10, '2024_08_28_231221_add_user_id_to_events_table', 1),
+(11, '2024_08_31_134025_create_event_user_table', 2);
 
 -- --------------------------------------------------------
 
@@ -159,7 +200,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('NU2LuqWAXT5N8KxvTVH1r6aZwFgH04rb9gyL4J50', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiNFJyenBWWldXc2VoZTVQSERTb2VGNG5PaVJ1NkpjaUV2Zzd1Wm5HNyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MzoidXJsIjthOjE6e3M6ODoiaW50ZW5kZWQiO3M6MzU6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9ldmVudHMvY3JlYXRlIjt9fQ==', 1724890861);
+('dUYLnChARCXLiHN2otYHXIsecqlnJhfuDd1yVA4X', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibWVKT0RaZVlNblVxWVRDalpKQlc1Tk1UMXVOeGVrV1RtV3dkemR5cCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9ldmVudHMvMyI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1725147323);
 
 -- --------------------------------------------------------
 
@@ -184,6 +225,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
+(1, 'Francisco Mateus', 'mateus@teste.com', NULL, '$2y$12$9xScvJpTPv.rIu14IHAQR.jKqSDwlQzS8ygVL9gYv7X.DiaOzGIwi', NULL, NULL, NULL, NULL, NULL, NULL, '2024-08-29 04:02:10', '2024-08-29 04:02:10'),
+(2, 'Roberto Lima', 'roberto@teste', NULL, '$2y$12$PjQdz8jU1GJTf.DjEOQ58eaaHcCP7J9CHKuoF4RVUKav0WHI0Cwba', NULL, NULL, NULL, NULL, NULL, NULL, '2024-08-31 21:53:23', '2024-08-31 21:53:23');
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -205,6 +254,13 @@ ALTER TABLE `cache_locks`
 ALTER TABLE `events`
   ADD PRIMARY KEY (`id`),
   ADD KEY `events_user_id_foreign` (`user_id`);
+
+--
+-- Índices de tabela `event_user`
+--
+ALTER TABLE `event_user`
+  ADD KEY `event_user_event_id_foreign` (`event_id`),
+  ADD KEY `event_user_user_id_foreign` (`user_id`);
 
 --
 -- Índices de tabela `migrations`
@@ -255,13 +311,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `personal_access_tokens`
@@ -279,7 +335,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restrições para tabelas despejadas
@@ -290,6 +346,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `events`
   ADD CONSTRAINT `events_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Restrições para tabelas `event_user`
+--
+ALTER TABLE `event_user`
+  ADD CONSTRAINT `event_user_event_id_foreign` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
+  ADD CONSTRAINT `event_user_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
